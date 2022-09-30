7dfc3ee4c895198f079a3ccacb564bc08e0e1c28
Manuel Wandeto <immanuelmojo@hotmail.com>
2022-09-30 22:32:39 +0300

    Update main.dart
    Add app-wide textTheme and sliderTheme for material slider component

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,26 @@ void main() {
     theme: ThemeData.dark().copyWith(
       appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF090C22)),
       scaffoldBackgroundColor: const Color(0xFF090C22),
-      textTheme: TextTheme(
-        bodyText2: const TextStyle(fontFamily: 'Nunito'), 
-        labelLarge: const TextStyle(fontFamily: 'Nunito')),
+        textTheme: const TextTheme(
+            bodyMedium: TextStyle(
+              fontFamily: 'Nunito',
+            ),
+            labelLarge: TextStyle(
+              fontFamily: 'Nunito',
+            ),
+            displayLarge: TextStyle(
+              fontFamily: 'Nunito',
+              fontWeight: FontWeight.w800,
+              fontSize: 54,
+            )),
+        sliderTheme: SliderThemeData(
+          activeTrackColor: Colors.white,
+          inactiveTrackColor: Colors.white.withOpacity(.4),
+          thumbColor: const Color(0xFFEA1556),
+          thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 14),
+          trackHeight: 2,
+          overlayColor: const Color(0xFF311330).withOpacity(.6),
+        ),
       colorScheme: ColorScheme.fromSwatch(
         accentColor: const Color(0xFFEA1556),
         cardColor: const Color(0xFF101427),
