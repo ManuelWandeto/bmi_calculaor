6ec018a73617dc740b6233ddb97841bd7e8cd8c9
Manuel Wandeto <immanuelmojo@hotmail.com>
2022-10-02 00:28:58 +0300

    Update gender_input.dart
    implement active and inactive UI states for gender_input

--- lib/components/gender_input.dart
+++ lib/components/gender_input.dart
@@ -1,30 +1,47 @@
 import 'package:flutter/material.dart';
 import 'card_box.dart';
+import '../constants.dart';
 
 enum Gender { male, female }
 
 class GenderInput extends StatelessWidget {
-  const GenderInput({super.key, required this.gender});
+  const GenderInput({
+    super.key,
+    required this.gender,
+    required this.onTap,
+    required this.isActive,
+  });
   final Gender gender;
+  final Function onTap;
+  final bool isActive;
+  
   @override
   Widget build(BuildContext context) {
-    return CardBox(
-      height: 170,
-      child: Align(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              gender == Gender.male ? Icons.male : Icons.female,
-              size: 72,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              gender == Gender.male ? 'MALE' : 'FEMALE',
-              style: Theme.of(context).textTheme.labelLarge,
-            ),
-          ],
+    return GestureDetector(
+      onTap: () => onTap(),
+      child: CardBox(
+        height: 170,
+        color: isActive ? kActiveCardColor : null,
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                gender == Gender.male ? Icons.male : Icons.female,
+                size: 72,
+                color: isActive ? Colors.white : kInactiveTextColor,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                gender == Gender.male ? 'MALE' : 'FEMALE',
+                style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      color: isActive ? Colors.white : kInactiveTextColor,
+                      fontWeight: isActive ? FontWeight.w600 : FontWeight.w400,
+                    ),
+              ),
+            ],
+          ),
         ),
       ),
     );
