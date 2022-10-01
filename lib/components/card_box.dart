152a70991ceafd16648c746223603e0a9d04254c
Manuel Wandeto <immanuelmojo@hotmail.com>
2022-10-01 21:17:33 +0300

    Update card_box.dart
    make height and color optional params

--- lib/components/card_box.dart
+++ lib/components/card_box.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
 class CardBox extends StatelessWidget {
-  const CardBox({super.key, required this.height, required this.child});
-  final double height;
+  const CardBox({
+    super.key,
+    this.height,
+    this.color = const Color(0xFF1D1F33),
+    required this.child,
+  });
+  final double? height;
+  final Color color;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: height,
-      decoration: const BoxDecoration(
-          color: Color(0xFF1D1F33),
-          borderRadius: BorderRadius.all(Radius.circular(10))),
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.all(Radius.circular(10))),
       child: child,
     );
   }
