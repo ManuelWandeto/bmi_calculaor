c25ce076ac255fd44beb460fbe52175a76094085
Manuel Wandeto <immanuelmojo@hotmail.com>
2022-10-01 21:23:27 +0300

    Update main.dart
    Add bodyLarge and headlineLarge properties to global text theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,29 @@ void main() {
       appBarTheme: const AppBarTheme(backgroundColor: Color(0xFF090C22)),
       scaffoldBackgroundColor: const Color(0xFF090C22),
       textTheme: TextTheme(
-          bodyMedium: const TextStyle(
-              fontFamily: 'Nunito',
-            ),
-            labelLarge: TextStyle(
-              fontFamily: 'Nunito',
-            fontSize: 16,
-            color: Colors.white.withOpacity(.8),
-            letterSpacing: 0.3,
-            ),
-          displayLarge: const TextStyle(
-              fontFamily: 'Nunito',
-              fontWeight: FontWeight.w800,
-              fontSize: 54,
-            )),
+        bodyMedium: const TextStyle(
+          fontFamily: 'Nunito',
+        ),
+        bodyLarge: const TextStyle(
+          fontFamily: 'Nunito',
+          fontSize: 21,
+        ),
+        labelLarge: TextStyle(
+          fontFamily: 'Nunito',
+          fontSize: 16,
+          color: Colors.white.withOpacity(.8),
+          letterSpacing: 0.3,
+        ),
+        displayLarge: const TextStyle(
+          fontFamily: 'Nunito',
+          fontWeight: FontWeight.w800,
+          fontSize: 54,
+        ),
+        headlineLarge: const TextStyle(
+          fontFamily: 'Nunito',
+          fontWeight: FontWeight.w600,
+        ),
+      ),
         sliderTheme: SliderThemeData(
           activeTrackColor: Colors.white,
           inactiveTrackColor: Colors.white.withOpacity(.4),
